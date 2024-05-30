7461a04ff800dedc1d69f73991cb79c9de0aed44
varshmaan-yaro <varshmaan.sonkar@gmail.com>
2024-05-30 17:29:09 +0530

    changed website name

--- src/views/components/footer/index.tsx
+++ src/views/components/footer/index.tsx
@@ -13,12 +13,9 @@ const Footer = () => {
 					<div>Stellar Coaching & Consulting</div>
 					<div>B-502, Sahara Apartment, Plot No. 11, Sector 6 Dwarka,</div>
 					<div>Dwarka, New Delhi, Delhi 110075, India</div>
-					<a
-						href='tel:+919654308000'
-						className='flex flex-row items-center text-[#b5c9dd] bg-blue-800 w-fit py-2 px-4 rounded-full mt-4'
-					>
+					<div className='flex flex-row items-center text-[#b5c9dd] bg-blue-800 w-fit py-2 px-4 rounded-full mt-4'>
 						<img src={PHONE} className='w-[15px] mr-2 mt-[2px]' /> +919654308000
-					</a>
+					</div>
 					<a
 						href='mailto:prashant@prashantvarma.com'
 						className='flex flex-row items-center text-[#b5c9dd] bg-blue-800 w-fit py-2 px-4 rounded-full mt-4'
@@ -28,13 +25,22 @@ const Footer = () => {
 				</div>
 				<div className='text-sm flex flex-col gap-2 w-full md:w-1/3'>
 					<div className='text-lg pb-4'>Pages</div>
-					<Link className='hover:text-slate-500 transition-[0.3s]' to='/'>Home</Link>
-					<Link className='hover:text-slate-500 transition-[0.3s]' to='/terms'>Terms & Conditions</Link>
-					<Link className='hover:text-slate-500 transition-[0.3s]' to='/privacy'>	Privacy Policy</Link>
-					<Link className='hover:text-slate-500 transition-[0.3s]' to='/disclaimer'>Disclaimer</Link>
+					<Link className='hover:text-slate-500 transition-[0.3s]' to='/'>
+						Home
+					</Link>
+					<Link className='hover:text-slate-500 transition-[0.3s]' to='/terms'>
+						Terms & Conditions
+					</Link>
+					<Link className='hover:text-slate-500 transition-[0.3s]' to='/privacy'>
+						{' '}
+						Privacy Policy
+					</Link>
+					<Link className='hover:text-slate-500 transition-[0.3s]' to='/disclaimer'>
+						Disclaimer
+					</Link>
 				</div>
 			</div>
-			<div className='bg-[#15232b] text-center mt-8'>©2024. All Rights Reserved.</div>
+			<div className='bg-[#15232b] text-center mt-8'>©2024 Propmatch. All Rights Reserved.</div>
 		</footer>
 	);
 };
--- src/views/pages/disclaimer/index.tsx
+++ src/views/pages/disclaimer/index.tsx
@@ -5,7 +5,7 @@ const Disclaimer = () => {
 				<div className='text-[2rem] font-medium mt-4'>Disclaimer</div>
 				<div className='mt-10'>
 					The information contained on this website is for general informational purposes only. The
-					information is provided by “prashantvarma.in” and while we strive to keep the information
+					information is provided by “propmatch.in” and while we strive to keep the information
 					up-to-date and correct, we make no representations or warranties of any kind, express or
 					implied, about the completeness, accuracy, reliability, suitability, or availability with
 					respect to the website or the information, properties, services, or related graphics
@@ -19,13 +19,13 @@ const Disclaimer = () => {
 				</div>
 				<div className='mt-10'>
 					Through this website, you may be able to link to other websites which are not under the
-					control of “prashantvarma.in”. We have no control over the nature, content, and
+					control of “propmatch.in”. We have no control over the nature, content, and
 					availability of those sites. The inclusion of any links does not necessarily imply a
 					recommendation or endorsement of the views expressed within them.
 				</div>
 				<div className='mt-10'>
 					Every effort is made to keep the website up and running smoothly. However,
-					“prashantvarma.in” takes no responsibility for, and will not be liable for, the website
+					“propmatch.in” takes no responsibility for, and will not be liable for, the website
 					being temporarily unavailable due to technical issues beyond our control.
 				</div>
 			</div>

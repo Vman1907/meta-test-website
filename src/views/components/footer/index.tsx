d11e2d7c88a690c764b4a45ea55119f698019124
Vman1907 <varshmaan.sonkar@gmail.com>
2025-01-26 00:12:01 +0530

    remove email contact from footer and navbar; adjust footer address formatting

--- src/views/components/footer/index.tsx
+++ src/views/components/footer/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { EMAIL, PHONE } from '../../../assets/images';
+import { PHONE } from '../../../assets/images';
 
 const Footer = () => {
 	return (
@@ -11,17 +11,11 @@ const Footer = () => {
 				<div className='text-sm w-full md:w-1/3'>
 					<div className='text-lg pb-4'>Contact Us</div>
 					<div>Stellar Coaching & Consulting</div>
-					<div>B-502, Sahara Apartment, Plot No. 11, Sector 6 Dwarka,</div>
+					<div>Plot No. 11, Sector 6 Dwarka,</div>
 					<div>Dwarka, New Delhi, Delhi 110075, India</div>
 					<div className='flex flex-row items-center text-[#b5c9dd] bg-blue-800 w-fit py-2 px-4 rounded-full mt-4'>
 						<img src={PHONE} className='w-[15px] mr-2 mt-[2px]' /> +919654308000
 					</div>
-					<a
-						href='mailto:prashant@prashantvarma.com'
-						className='flex flex-row items-center text-[#b5c9dd] bg-blue-800 w-fit py-2 px-4 rounded-full mt-4'
-					>
-						<img src={EMAIL} className='w-[15px] mr-2 mt-[2px]' /> prashant@prashantvarma.com
-					</a>
 				</div>
 				<div className='text-sm flex flex-col gap-2 w-full md:w-1/3'>
 					<div className='text-lg pb-4'>Pages</div>
--- src/views/components/navbar/index.tsx
+++ src/views/components/navbar/index.tsx
@@ -2,18 +2,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-			<nav className={`bg-white text-black sticky top-0 right-0 left-0`}>
+			<nav className={`bg-white text-black sticky top-0 right-0 left-0 z-10`}>
 				<div className={`container mx-auto flex justify-between items-center p-4`}>
 					<Link to='/'>
 						<h1 className={`text-xl font-medium`}>Propmatch</h1>
 					</Link>
-					<div className='hidden md:block'>
-						<ul className={`flex space-x-4`}>
-							<li>
-								<a href='mailto:prashant@prashantvarma.com'>prashant@prashantvarma.com</a>
-							</li>
-						</ul>
-					</div>
 				</div>
 			</nav>
 		);
